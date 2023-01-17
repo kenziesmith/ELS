1469a92574d7da7c3671cf59e7fc829e7f6e66bf
Kenzie Smith <74722329+kenziesmith@users.noreply.github.com>
2023-01-17 05:24:47 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
